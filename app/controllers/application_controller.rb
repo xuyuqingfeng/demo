08e8a1c015b8ac5bfc371b10b74c7dde3f0e755f
xjy_blockheaders <xjy_blockheaders@blockheaders.com>
2018-04-11 16:42:14 +0800

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # def after_sign_in_path_for(resource)
   #   if resource.is_a?(User)
-  #     if User.count == 2
-  #       resource.add_role 'admin'
+  #     if User.count == 3
+  #       resource.add_role 'user'
   #     end
   #     resource
   #   end
